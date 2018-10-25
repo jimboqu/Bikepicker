4a01f2014f38cf43cb64a41da838fb4ebf4acea6
jimboqu <jamesm.quilter@gmail.com>
2018-10-25 20:59:32 +0100

    added boostrap nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
         (link_to "Sign up", new_user_registration_path)  
       end 
 	end
+
+    def logged_in_nav_helper
+      if user_signed_in? 
+        link_to "My Bikes", user_bikes_index_path(current_user.id), class: "nav-link"
+        link_to "Log out", destroy_user_session_path, method: :delete, class: "nav-link"
+        else 
+        (link_to "Sign in", new_user_session_path, class: "nav-link") +
+        (link_to "Register", new_user_registration_path, class: "nav-link")
+      end
+    end
+	      
 end
